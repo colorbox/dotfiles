cc385acddb45e0fbdf9cf9d516096498cf9a970c
colorbox <colorbox222@gmail.com>
2018-07-04 23:11:01 +0900

    Add gitignore

--- deploy.sh
+++ deploy.sh
@@ -4,3 +4,4 @@ ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.pryrc ~/.pryrc
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/.gitignore ~/.gitignore
